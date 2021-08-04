e5631e7abb644fd0b4d0c2244da28740b31abdd6
alanlhc <alan.low@avnet.com>
2021-08-04 14:28:06 +0800

    Update setup-project.sh.

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+#NOTE: This script is to be used in the Linux Github Action workflow.
+
 set -e
 
 pushd "$(dirname $0)"/../iotc-azsphere-sdk
+
 # initial cleanup
 
 rm -rf iotc-c-lib cJSON
 
 # clone the dependency repos
 git clone --depth 1 --branch protocol-2.0 git://github.com/Avnet/iotc-c-lib.git
-git clone --depth 1 --branch v1.7.13 git://github.com/DaveGamble/cJSON.git
+git clone --depth 1 --branch git://github.com/DaveGamble/cJSON.git
 
 popd >/dev/null
 echo Done
